07a3e2f519327a87447fd77cdf5106b12e2cb219
loxi25 <oscarsanchez1@colegioguanenta.edu.co>
2025-02-12 08:38:38 -0500

    se crearon dos archivos

--- juego.py
+++ juego.py
@@ -1,22 +1,51 @@
-print("seleccione un numero")
+ # importamos la libreria random
+import random
 
-print("1. piedra")
-print("2. papel")
-print("3. tijeras")
+print("-------------------------------")
+print("--- Piedra, papel y tijeras ---")
+print("-------------------------------")
+print("1. Piedra")
+print("2. Papel")
+print("3. Tijeras")
+print("-------------------------------")
 
-n=input("selecciona el objeto que desees")
-z=random(1.0,2.0,3.0)
-if  n==z:
-    r=empate
-if  n==1 and z==2:
-    r=perdiste
-if  n==1 and z==3:
-    r=ganaste
-if  n==2 and z==3:
-    r=perdiste
-if  n==2 and z==1:
-    r=ganaste
-if  n==3 and z==2:
-    r=ganaste
-if  n==3 and z==1:
-    r=perdiste
+# input
+usuario =int(input("Digite la opción deseada: "))
+
+# processing
+maquina = random.randint(1,3)
+
+if usuario < 1 or usuario > 4:
+    print("Por favor juegue nuevamente")
+    r = "Opción no válida."
+else:
+    # el usuario escogió una opción válida
+    if maquina == 1:
+        if usuario == 1:
+            r = "Empate"
+        elif usuario == 2:
+            r = "Ganaste"
+        else:
+            r = "Perdiste"
+    elif maquina == 2:
+        if usuario == 1:
+            r = "Perdiste"
+        elif usuario == 2:
+            r = "Empate"
+        else:
+            r = "Ganaste"
+    else:
+        if usuario == 1:
+            r = "Perdiste"
+        elif usuario == 2:
+            r = "Ganaste"
+        else:
+            r = "Empate"
+
+# salida
+print("-------------------------------")
+print("---------Resultado ------------")
+print("--- " + r + " ----")
+print("Usuario: " + str(usuario))
+print("Máquina: " + str(maquina))
+print("-------------------------------")
